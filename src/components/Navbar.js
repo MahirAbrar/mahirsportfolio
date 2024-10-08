e105e0918107c11d97e6877282fe286912c3e1b8
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2024-10-08 18:40:54 +1100

    remove drop down

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,25 +31,8 @@ const Navbar = ({ theme, handleToggle }) => {
       } bg-base-100 text-base-content`}
     >
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-        </div>
-        <a href="#top" className="btn btn-ghost text-xl">
+        <a href="#top" className="btn btn-ghost text-xl flex items-center">
+          <img src="/favicon.ico" alt="Portfolio Icon" className="w-6 h-6" />
           Mahir's Portfolio
         </a>
       </div>
